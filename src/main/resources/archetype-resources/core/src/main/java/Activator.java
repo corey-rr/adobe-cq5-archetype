7fa5a65487c46aecf239d2935abdf221f650fe10
david <github@activecq.com>
2012-08-12 21:00:01 -0400

    Initial commit

---
+++ src/main/resources/archetype-resources/core/src/main/java/Activator.java
@@ -0,0 +1,33 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Activator implements BundleActivator {
+
+    /**
+     * default logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(Activator.class);
+
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator${symbol_pound}start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        log.info(context.getBundle().getSymbolicName() + " started");
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator${symbol_pound}stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        log.info(context.getBundle().getSymbolicName() + " stopped");
+    }
+}
